e87ddafa2ff6f9a74195794eea7b5b8c71bf569a
Coder Tester <codewithme@outlook.com>
2016-02-27 01:30:28 +0530

    first commit

---
+++ src/test/java/com/demotours/mercury/unit/UnitTests.java
@@ -0,0 +1,15 @@
+package com.demotours.mercury.unit;
+
+import org.testng.annotations.Test;
+
+public class UnitTests {
+
+	@Test
+	public void firstTest(){
+		
+	}
+	@Test
+	public void secondTest(){
+		
+	}
+}
